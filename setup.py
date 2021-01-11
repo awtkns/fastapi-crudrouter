1100aea6e531d4d15f74ede90cef2c56cd6fa039
Adam <cadamrun@gmail.com>
2021-01-11 13:49:18 -0800

    :bookmark: Bump version to 0.3.2 fixing #21

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fastapi-crudrouter',
-    version='0.3.1',
+    version='0.3.2',
     author='Adam Watkins',
     author_email='cadamrun@gmail.com',
     packages=find_packages(exclude=('tests', 'tests.implementations')),
