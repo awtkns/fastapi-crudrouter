15ad18283afc03f62fc32398a9d798a631cc7ccb
Adam <cadamrun@gmail.com>
2021-02-01 19:49:17 -0800

    :white_check_mark: Updating tests

--- tests/implementations/__init__.py
+++ tests/implementations/__init__.py
@@ -2,7 +2,6 @@ from .memory import memory_implementation
 from .sqlalchemy_ import sqlalchemy_implementation, sqlalchemy_implementation_custom_ids, sqlalchemy_implementation_string_pk
 from .overloaded import overloaded_app
 from .databases_ import databases_implementation, databases_implementation_custom_ids, databases_implementation_string_pk
-from .tortoise_ import tortoise_implementation
 
 implementations = [
     memory_implementation,
