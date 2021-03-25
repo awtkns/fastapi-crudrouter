2347df5ac92177bb3b6d5055f764252a9d849650
Adam Watkins <cadamrun@gmail.com>
2021-03-24 23:49:41 -0700

    :white_check_mark: Add Update Integrity Error Tests

--- tests/test_sqlalchemy_router.py
+++ tests/test_sqlalchemy_router.py
@@ -1,16 +1,14 @@
 import pytest
 
-from sqlalchemy import Column, String, Float, Integer
-from sqlalchemy.exc import IntegrityError
-
 from fastapi.testclient import TestClient
+from sqlalchemy import Column, String, Float, Integer
+
 from fastapi_crudrouter import SQLAlchemyCRUDRouter
-
-from tests.implementations.sqlalchemy_ import _setup_base_app
-from tests import Potato, Carrot, CarrotCreate, CarrotUpdate
+from tests import Potato, Carrot, CarrotUpdate
 from tests import test_router
+from tests.implementations.sqlalchemy_ import _setup_base_app
 
-
+POTATO_URL = '/potato'
 
 
 def get_app():
@@ -21,7 +19,7 @@ def get_app():
         id = Column(Integer, primary_key=True, index=True)
         thickness = Column(Float)
         mass = Column(Float)
-        color = Column(String)
+        color = Column(String, unique=True)
         type = Column(String)
 
     class CarrotModel(Base):
@@ -47,7 +45,7 @@ def test_integrity_error():
         type='russet'
     )
 
-    args = client, '/potato', potato
+    args = client, POTATO_URL, potato
     test_router.test_post(*args)
     with pytest.raises(AssertionError):
         test_router.test_post(*args)
@@ -56,3 +54,34 @@ def test_integrity_error():
     args = client, '/carrot', Carrot(id=1, length=2, color='red')
     test_router.test_post(*args)
     test_router.test_post(*args, expected_length=2)
+
+
+def test_integrity_error_update():
+    client = TestClient(get_app())
+    potato1 = Potato(
+        id=1,
+        thickness=2,
+        mass=5,
+        color='red',
+        type='russet'
+    )
+
+    potato2 = Potato(
+        id=2,
+        thickness=9,
+        mass=5,
+        color='yellow',
+        type='mini'
+    )
+
+    args = client, POTATO_URL
+    test_router.test_post(*args, potato1, expected_length=1)
+    test_router.test_post(*args, potato2, expected_length=2)
+
+    potato2.color = potato1.color
+    res = client.put(f'{POTATO_URL}/{potato2.id}', json=potato2.dict())
+    assert res.status_code == 422, res.json()
+
+    potato2.color = 'green'
+    res = client.put(f'{POTATO_URL}/{potato2.id}', json=potato2.dict())
+    assert res.status_code == 200, res.json()
