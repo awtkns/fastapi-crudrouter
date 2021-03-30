7db9344842b9b0e6c2dac2458a29a6de532ceb8c
Adam Watkins <cadamrun@gmail.com>
2021-03-30 15:06:38 -0700

    :white_check_mark: Custom Tags Tests

---
+++ tests/test_openapi_schema.py
@@ -0,0 +1,34 @@
+import pytest
+
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from fastapi_crudrouter import MemoryCRUDRouter
+
+from tests import Potato, Carrot, CUSTOM_TAGS
+
+POTATO_TAGS = ["Potato"]
+PATH_TAGS = {
+    "/potato": POTATO_TAGS,
+    "/potato/{item_id}": POTATO_TAGS,
+    "/carrot": CUSTOM_TAGS,
+    "/carrot/{item_id}": CUSTOM_TAGS,
+}
+
+
+class TestOpenAPISpec:
+    def test_schema_exists(self, client):
+        res = client.get("/openapi.json")
+        assert res.status_code == 200
+
+        return res
+
+    def test_schema_tags(self, client):
+        schema = self.test_schema_exists(client).json()
+        paths = schema["paths"]
+
+        assert len(paths) == len(PATH_TAGS)
+        for path, method in paths.items():
+            assert len(method) == 3
+
+            for m in method:
+                assert method[m]["tags"] == PATH_TAGS[path]
