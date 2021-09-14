e5a645560f721e620f5f97cec2e012b7ebd6b8fb
Adam Watkins <cadamrun@gmail.com>
2021-09-13 22:24:44 -0700

    :white_check_mark: Add test for disabling a single route

--- tests/test_dependencies/test_disable.py
+++ tests/test_dependencies/test_disable.py
@@ -1,12 +1,11 @@
-from fastapi import Depends, HTTPException
-from fastapi.security import OAuth2PasswordBearer
-
 import pytest
 
 from fastapi_crudrouter.core import CRUDGenerator
 
 from tests.implementations import implementations
-from tests.conftest import yield_test_client
+from tests.conftest import yield_test_client, label_func
+from tests import test_router
+
 
 URLS = ["/potato", "/carrot"]
 AUTH = {"Authorization": "Bearer my_token"}
@@ -14,12 +13,22 @@ KEY_WORDS = {f"{r}_route" for r in CRUDGenerator.get_routes()}
 DISABLE_KWARGS = {k: False for k in KEY_WORDS}
 
 
-@pytest.fixture(params=implementations, scope="class")
+@pytest.fixture(params=implementations, ids=label_func, scope="class")
 def client(request):
     impl, dsn = request.param
 
     app, router, settings = impl(db_uri=dsn)
     [app.include_router(router(**s, **DISABLE_KWARGS)) for s in settings]
+
+    yield from yield_test_client(app, impl)
+
+
+@pytest.fixture(params=implementations, ids=label_func, scope="class")
+def delete_all_client(request):
+    impl, dsn = request.param
+
+    app, router, settings = impl(db_uri=dsn)
+    [app.include_router(router(**s, delete_all_route=False, update_route=False)) for s in settings]
 
     yield from yield_test_client(app, impl)
 
@@ -34,3 +43,13 @@ def test_route_disable(client, url):
         assert client.get(f"{url}/{id_}").status_code == 404
         assert client.put(f"{url}/{id_}").status_code == 404
         assert client.delete(f"{url}/{id_}").status_code == 404
+
+
+def test_route_disable_single(delete_all_client):
+    url = "/potato"
+
+    assert delete_all_client.delete(url).status_code == 405
+
+    test_router.test_post(delete_all_client, url)
+    assert delete_all_client.put(f"{url}/{1}").status_code == 405
+    assert delete_all_client.delete(f"{url}/{1}").status_code == 200
