9fafe29d83002650141e4a6fbf99f82b53e65074
awtkns <cadamrun@gmail.com>
2022-01-27 19:02:38 -0800

    :rocket: Update release doc generator to show changelog

--- .github/scripts/releases.py
+++ .github/scripts/releases.py
@@ -1,12 +1,13 @@
 import re
 from os import environ
 
-from github import Github, ContentFile
+from github import Github
 from github.GitRelease import GitRelease
 
 GITHUB_REPOSITORY = environ.get("GITHUB_REPOSITORY", "awtkns/fastapi-crudrouter")
 GITHUB_TOKEN = environ.get("GH_TOKEN") or environ.get("GITHUB_TOKEN")
 GITHUB_URL = "https://github.com"
+GITHUB_BRANCH = "master"
 FILE_PATH = "docs/en/docs/releases.md"
 COMMIT_MESSAGE = "🤖 auto update releases.md"
 
@@ -14,10 +15,11 @@ COMMIT_MESSAGE = "🤖 auto update releases.md"
 gh = Github(GITHUB_TOKEN)
 
 
-def generate_header(r: GitRelease, separator: bool = False):
-    header = ""
-    if separator:
-        header += "\n\n---\n"
+def generate_header(r: GitRelease, header_row: bool = False):
+    if header_row:
+        header = "Release Notes\n===\n"
+    else:
+        header = "\n\n---\n"
 
     return (
         header
@@ -37,13 +39,23 @@ def commit_update(content: str):
     else:
         print("Uploading new release documentation")
 
-    repo.update_file(file.path, message=COMMIT_MESSAGE, content=content, sha=file.sha)
+    repo.update_file(
+        file.path,
+        message=COMMIT_MESSAGE,
+        content=content,
+        sha=file.sha,
+        branch=GITHUB_BRANCH,
+    )
 
 
 def insert_links(content: str):
     """Replaces both #pull and @author with correct links"""
-    url = repo.html_url + "/pull"
-    content = re.sub(r"#(\d+)", rf"[#\1]({url}/\1)", content)
+    pull_url = repo.html_url + "/pull"
+    content = re.sub(r"#(\d+)", rf"[#\1]({pull_url}/\1)", content)
+
+    compare_url = repo.html_url + "/compare"
+    content = re.sub(rf"{compare_url}/([^\s]+)", rf"[`\1`]({compare_url}/\1)", content)
+
     return re.sub(r"@(\S+)", rf"[@\1]({GITHUB_URL}/\1)", content)
 
 
@@ -51,12 +63,12 @@ if __name__ == "__main__":
     repo = gh.get_repo(GITHUB_REPOSITORY)
 
     new_content = ""
-    first = False
-    for r in repo.get_releases():
-        if not r.draft:
-            new_content += generate_header(r, first)
-            new_content += r.body
-            first = True
+    show_header = True
+    for release in repo.get_releases():
+        if not release.draft:
+            new_content += generate_header(release, header_row=show_header)
+            new_content += release.body
+            show_header = False
 
     new_content = insert_links(new_content)
     commit_update(new_content)
